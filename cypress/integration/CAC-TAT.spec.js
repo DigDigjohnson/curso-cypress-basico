65aa8dab57bb03d54d02368b0d5e1f0a570c7f94
DigDigJohnson <rodrigoomlehnen@gmail.com>
2024-08-24 09:43:10 -0300

    final files project

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,24 +1,35 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
+    const timer = 3000
     beforeEach(function () {
         cy.visit('./src/index.html')
     })
     it('verifica o titulo da aplicacao', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
-    it('preenche os campos obrigatorios e envia o formulario', function () {
-        const longText = 'Texto longo para teste de insercao de grandes quantidades de caracteres dentro do campo de texto com delay modificado na insercao'
+    Cypress._.times(5, function () {
+        it('preenche os campos obrigatorios e envia o formulario', function () {
+            const longText = 'Texto longo para teste de insercao de grandes quantidades de caracteres dentro do campo de texto com delay modificado na insercao'
 
-        cy.get('#firstName').type('Rodrigo')
-        cy.get('#lastName').type('Lehnen')
-        cy.get('#email').type('rodrigoomlehnen@gmail.com')
-        cy.get('#open-text-area').type(longText, { delay: 0 })
-        cy.contains('button', 'Enviar').click()
-        cy.get('.success').should('be.visible')
+            cy.clock()
+
+            cy.get('#firstName').type('Rodrigo')
+            cy.get('#lastName').type('Lehnen')
+            cy.get('#email').type('rodrigoomlehnen@gmail.com')
+            cy.get('#open-text-area').type(longText, { delay: 0 })
+            cy.contains('button', 'Enviar').click()
+            cy.get('.success').should('be.visible')
+
+            cy.tick(timer)
+
+            cy.get('.success').should('not.be.visible')
+        })
     })
     it('exibe mensagem de erro ao submeter o formulario com um email com formatacao invalida', function () {
         const longText = 'Texto longo para teste de insercao de grandes quantidades de caracteres dentro do campo de texto com delay modificado na insercao'
+
+        cy.clock()
 
         cy.get('#firstName').type('Rodrigo')
         cy.get('#lastName').type('Lehnen')
@@ -26,6 +37,8 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#open-text-area').type(longText, { delay: 0 })
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
+        cy.tick(timer)
+        cy.get('.error').should('not.be.visible')
     })
     it('validar se o campo de telefone nao aceita caracteres nao-numericos', function () {
 
@@ -33,10 +46,15 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#phone').type('!@#$%¨&*()_-+=').should('not.have.text')
     })
     it('exibe mensagem de erro quando o telefone se torna obrigatorio mas nao e preenchido antes do envio do formulario', function () {
+
+        cy.clock()
+
         cy.get('#phone-checkbox').check()
         cy.get('.phone-label-span').should('be.visible')
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
+        cy.tick(timer)
+        cy.get('.error').should('not.be.visible')
     })
     it('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
         const longText = 'Texto longo para teste de insercao de grandes quantidades de caracteres dentro do campo de texto com delay modificado na insercao'
@@ -47,12 +65,22 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#open-text-area').type(longText, { delay: 0 }).should('have.value', longText).clear().should('have.value', '')
     })
     it('exibe mensagem de erro ao submeter o formulario sem preencher os campos obrigatorios', function () {
+
+        cy.clock()
+
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
+        cy.tick(timer)
+        cy.get('.error').should('not.be.visible')
     })
     it('envia o formulario com sucesso usando um comando customizado', function () {
+
+        cy.clock()
+
         cy.fillMandatoryFieldsAndSubmit()
         cy.get('.success').should('be.visible')
+        cy.tick(timer)
+        cy.get('.success').should('not.be.visible')
     })
     it('seleciona um produto (YouTube) por seu texto', function () {
         cy.get('select').select('YouTube').should('have.value', 'youtube')
@@ -105,5 +133,52 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     it('acessa a pagina da politica de privacidade removendo o target e entao clicando no link', function () {
         cy.get('#privacy a').invoke('removeAttr', 'target').click()
         cy.contains('Talking About Testing').should('be.visible')
+    })
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke()', function () {
+        cy.get('.success')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Mensagem enviada com sucesso.')
+            .invoke('hide')
+            .should('not.be.visible')
+        cy.get('.error')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Valide os campos obrigatorios!')
+            .invoke('hide')
+            .should('not.be.visible')
+    })
+    it('preenche a area de texto usando o comando invoke', function () {
+        const longText = 'Texto longo para teste de insercao de grandes quantidades de caracteres dentro do campo de texto com delay modificado na insercao'
+
+        cy.clock()
+
+        cy.get('#firstName').type('Rodrigo')
+        cy.get('#lastName').type('Lehnen')
+        cy.get('#email').type('rodrigoomlehnen@gmail.com')
+        cy.get('#open-text-area').invoke('val', longText).should('have.value', longText)
+        cy.contains('button', 'Enviar').click()
+        cy.get('.success').should('be.visible')
+
+        cy.tick(timer)
+
+        cy.get('.success').should('not.be.visible')
+    })
+
+    it('faz uma requisicao HTTP', function () {
+        cy.request('GET', 'https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .then(function (response) {
+                expect(response.status).to.equal(200);
+                expect(response.statusText).to.equal('OK');
+                expect(response.body).include('CAC TAT')
+        })
+    })
+    it('Encontre o Gato', function () {
+        cy.get('#cat')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
     })
 })
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,6 @@
-it('testa a pagina da politica de privacidade de forma independente', function () {
-    cy.visit('src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
+Cypress._.times(3, function () {
+    it('testa a pagina da politica de privacidade de forma independente', function () {
+        cy.visit('src/privacy.html')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
